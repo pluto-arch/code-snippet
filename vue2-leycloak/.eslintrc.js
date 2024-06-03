d460fbbaa6da1f23f359f63c6ae40c350123c09b
mccoy_zhang <1976935046@qq.com>
2024-06-03 20:32:32 +0800

    添加vue2 keycloak示例

---
+++ vue2-leycloak/.eslintrc.js
@@ -0,0 +1,69 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true
+	},
+	extends: ['plugin:vue/essential', 'eslint:recommended', 'plugin:prettier/recommended'],
+	parserOptions: {
+		parser: '@babel/eslint-parser'
+	},
+	rules: {
+		'prettier/prettier': [
+			'error',
+			{
+				requireConfig: false,
+				endOfLine: 'auto',
+				quoteProps: 'as-needed',
+				proseWrap: 'preserve',
+				arrowParens: 'always',
+				htmlWhitespaceSensitivity: 'strict',
+				ignorePath: '.prettierignore',
+				jsxBracketSameLine: false,
+				jsxSingleQuote: false,
+				vueIndentScriptAndStyle: true,
+				semi: false,
+				trailingComma: 'none',
+				singleQuote: true,
+				printWidth: 150,
+				tabWidth: 2,
+				bracketSpacing: true,
+				useTabs: true
+			}
+		],
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'vue/v-on-event-hyphenation': 'off',
+		'vue/multi-word-component-names': ['off'],
+		'vue/prop-name-casing': 'off',
+		'vue/require-default-prop': 'off',
+		'vue/no-v-html': 'off',
+		'no-new': 'off',
+		'prefer-regex-literals': 'off',
+		'prefer-promise-reject-errors': 'off',
+		'no-unused-vars': [
+			'off',
+			{
+				caughtErrors: 'none'
+			}
+		],
+		'vue/no-unused-vars': [
+			'off',
+			{
+				caughtErrors: 'none'
+			}
+		],
+		'no-tabs': 'off',
+		'no-trailing-spaces': 'off',
+		'no-mixed-spaces-and-tabs': 'off',
+		'no-empty-function': 'off',
+		'space-before-function-paren': ['off', 'always'],
+		'no-unreachable-loop': 'off',
+		'no-multiple-empty-lines': 'off',
+		'no-loss-of-precision': 'off',
+		'no-useless-escape': 0,
+		semi: ['warn', 'never'],
+		eqeqeq: 0,
+		indent: ['off', 2],
+		quotes: ['error', 'single', { allowTemplateLiterals: true }]
+	}
+}
