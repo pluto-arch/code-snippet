fbc36d41cd558ae323ce3764e0b51e2a301c0e99
mccoy_zhang <1976935046@qq.com>
2024-07-22 22:08:33 +0800

    add demo

---
+++ HttpDiagnostic/WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public async Task<string> Get()
+        {
+            using var client = new HttpClient();
+            var re = await client.GetAsync("https://localhost:6001/home/TestApi?id=123123&token=asdasdasd");
+            return await re.Content.ReadAsStringAsync();
+        }
+    }
+}
